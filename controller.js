3131a2847fcd8e43eae41eb53e4d1ce93b5b0f25
europayer <europayer@gmx.net>
2015-03-10 09:40:54 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -262,7 +262,7 @@ $().ready(function() {
 	var callerUrl = decodeURIComponent(location.href).split('&')[0].split('https://rawgit.com/europayer/pebble/master/')[1];
 	alert(callerUrl.indexOf('configuration.html?'));
 	if(callerUrl.indexOf('configuration.html?')){
-		callerUrl = callerUrl.split('configuration.html?')[1];
+		callerUrl = callerUrl.split('?')[1];
 		alert('CallerULR: ' + callerUrl);
 	}
 	alert('CallerULR: ' + callerUrl);
